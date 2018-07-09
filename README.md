f3c1381feb11e1dd18e726afab260e3bb9329c3d
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-07-09 11:33:39 +0300

    feat: fetching results from API: OK, added comments regarding potential errors during developemnt process as well as their corrections

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ The tutorial is in French.
 ## Differences
 
 - This app uses `createBottomTabNavigator` instead of `TabNavigator`.
+
+## Erros and learnt lessons
+
+This section provides a list of the potential errors that happened during the development process, as well as, their correction:
+
+- `Invariant Violation: Tried to get frame for out of range index NaN`: This is due to the fact that an object was provided to the FlatList `data` prop, more precisely the `response.data` from the axios get method. To correct it ensure that it is the list with results and not the object: `response.data` --> `response.data.list`
--- components/Result.js
+++ components/Result.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  ListView,
+  FlatList
+} from "react-native";
+import axios from "axios";
+
+import globalStyles from "../Styles";
+
 class Result extends Component {
   static navigationOptions = ({ navigation }) => {
     //console.log("PARAMS : ", params);
@@ -14,13 +24,35 @@ class Result extends Component {
       city: "Sofia", //this.props.navigation.state.params.city,
       report: null
     };
+    this.fetchWeather();
   }
+
+  fetchWeather = () => {
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=${
+          this.state.city
+        }&mode=json&units=metric&cnt=12&APPID=7ad755a5c950ed324a9376b774cd75db`
+      )
+      .then(response => {
+        this.setState({
+          report: response.data
+        });
+      });
+  };
+
   render() {
-    return (
-      <View>
-        <Text>Result</Text>
-      </View>
-    );
+    if (this.state.report === null) {
+      return <ActivityIndicator color={globalStyles.color} size="large" />;
+    } else {
+      console.log("REPORT: ", this.state.report);
+      return (
+        <FlatList
+          data={this.state.report.list}
+          renderItem={({ item }) => <Text>{item.main.temp}</Text>}
+        />
+      );
+    }
   }
 }
 
